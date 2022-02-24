97f21486f2db333f1014586c2a4b2a9a4f485af2
Thomas R <contact@xrths.fr>
2022-02-24 10:39:36 +0100

    Menu + Boutons page d'accueuil

--- app/src/main/java/com/example/mycarbonfootprint/MainActivity.java
+++ app/src/main/java/com/example/mycarbonfootprint/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.mycarbonfootprint;
 
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private ImageButton voiture;
+    private ImageButton moto;
+    private ImageButton train;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        voiture = (ImageButton) findViewById(R.id.boutonVoiture);
+        voiture.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        moto = (ImageButton) findViewById(R.id.boutonMoto);
+        moto.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        train = (ImageButton) findViewById(R.id.boutonTrain);
+        train.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu m) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, m);
+        return true;
+    }
+
+
 }
